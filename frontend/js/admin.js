85c78f66f8440931b58e199cac6f1eeb1722ae05
Helen Liu <mail2helenl@gmail.com>
2024-11-01 23:26:09 -0700

    updated backend function to include req, updated admin.js logic to follow backend response format

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,32 +1,39 @@
 // admin.js
 document.addEventListener("DOMContentLoaded", () => {
     const apiUrl = serverEndpoints.getAllUsersData; // Replace with the actual endpoint URL
+    const token = localStorage.getItem("token");
   
     // Fetch data from the API
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => displayUserData(data))
-      .catch(error => console.error("Error fetching user data:", error));
-  });
+    fetch(apiUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          // Check if response is an error
+          if (data.message) {
+            throw new Error(data.message); // Handle unauthorized error
+          }
+          displayUserData(data);
+        })
+        .catch(error => console.error("Error fetching user data:", error));
+    });
   
   // Function to display user data on the admin page
   function displayUserData(data) {
-    // Get the container where the data will be displayed
+    if (!Array.isArray(data)) {
+      console.error("Data is not an array:", data);
+      return;
+    }
+  
     const container = document.querySelector(".container");
   
-    // Create the title and description
-    const title = document.createElement("h1");
-    title.classList.add("mb-3");
-    title.textContent = "Administration Page";
-  
-    const description = document.createElement("p");
-    description.textContent = "Keep track of the number of free calls made by users";
-  
-    // Create a table to display the user data
     const table = document.createElement("table");
     table.classList.add("table", "table-bordered", "mt-4");
   
-    // Create table headers
     const headers = `<thead class="table-dark">
                        <tr>
                          <th>Email</th>
@@ -35,21 +42,15 @@ document.addEventListener("DOMContentLoaded", () => {
                      </thead>`;
     table.innerHTML = headers;
   
-    // Create table body with data
     const tbody = document.createElement("tbody");
     data.forEach(user => {
       const row = `<tr>
                      <td>${user.email}</td>
-                     <td>${user.apiRequestCount}</td>
+                     <td>${user.number_of_requests}</td>
                    </tr>`;
       tbody.innerHTML += row;
     });
   
     table.appendChild(tbody);
-  
-    // Append title, description, and table to the container
-    container.appendChild(title);
-    container.appendChild(description);
     container.appendChild(table);
   }
-  
