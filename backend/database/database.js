adf4f9b555cb38cd70de7be9db5ba628458df3eb
Brett Reader <breader1@my.bcit.ca>
2024-11-15 19:40:38 -0800

    DB revamped and endpoints updated to account for it

--- backend/database/database.js
+++ backend/database/database.js
@@ -7,7 +7,20 @@
 "use strict";
 
 const sqlite3 = require("sqlite3").verbose();
+const { STATUS_CODES } = require("http");
 const messages = require("../localization/en/user.js");
+
+const Role = {
+  Admin: 1,
+  User: 2,
+};
+
+const Method = {
+  GET: 1,
+  POST: 2,
+  PATCH: 3,
+  DELETE: 4,
+};
 
 class Database {
   constructor(dbFilePath = ":memory:") {
@@ -24,8 +37,36 @@ class Database {
   async initializeTables() {
     try {
       await this.createUserTable();
-      await this.createServiceTable();
+      console.log("User table created");
+
+      await this.createRequestTable();
+      console.log("Request table created");
+
+      await this.createRoleTable();
+      console.log("Role table created");
+
+      await this.createMethodTable();
+      console.log("Method table created");
+
+      await this.createEndpointTable();
+      console.log("Endpoint table created");
+
+      await this.seedRoleTable();
+      console.log("Role table seeded");
+
       await this.seedUserTable();
+      console.log("User table seeded");
+
+      await this.seedMethodTable();
+      console.log("Method table seeded");
+
+      await this.seedEndpointTable();
+      console.log("Endpoint table seeded");
+
+      await this.seedRequestTable();
+      console.log("Request table seeded");
+
+      console.log(messages.database.success.initialization);
     } catch (err) {
       console.error(messages.database.errors.table_creation, err);
     }
@@ -36,22 +77,37 @@ class Database {
     await this.run(sql);
   }
 
-  async createServiceTable() {
+  async createRequestTable() {
     const sql = messages.database.queries.create.request_table;
+    await this.run(sql);
+  }
+
+  async createRoleTable() {
+    const sql = messages.database.queries.create.role_table;
+    await this.run(sql);
+  }
+
+  async createMethodTable() {
+    const sql = messages.database.queries.create.method_table;
+    await this.run(sql);
+  }
+
+  async createEndpointTable() {
+    const sql = messages.database.queries.create.endpoint_table;
     await this.run(sql);
   }
 
   async seedUserTable() {
     const users = [
       {
-        email: messages.database.default_user_email,
-        password: messages.database.default_user_password,
-        role: 0,
-      },
-      {
         email: messages.database.admin_user_email,
         password: messages.database.admin_user_password,
-        role: 1,
+        role: Role.Admin, // role_id 1 is Admin
+      },
+      {
+        email: messages.database.default_user_email,
+        password: messages.database.default_user_password,
+        role: Role.User, // role_id 2 is User
       },
     ];
 
@@ -70,10 +126,10 @@ class Database {
 
         // Insert the user into the database
         await this.run(messages.database.queries.insert.user, [
+          user.role,
           user.email,
           hashedPassword,
           salt,
-          user.role,
         ]);
         console.log(
           messages.database.success.user_insert.replace("{email}", user.email)
@@ -83,6 +139,208 @@ class Database {
           messages.database.warnings.skipped_user_insert.replace(
             "{email}",
             user.email
+          )
+        );
+      }
+    }
+  }
+
+  // Seed Role Table
+  async seedRoleTable() {
+    const roles = [
+      {
+        role_name: "Admin",
+      },
+      {
+        role_name: "User",
+      },
+    ];
+
+    for (const role of roles) {
+      // Check if role already exists
+      const existingRole = await this.get(
+        messages.database.queries.select.check_role_exists,
+        [role.role_name]
+      );
+
+      if (!existingRole) {
+        // Insert the role into the database
+        await this.run(messages.database.queries.insert.role, [role.role_name]);
+        console.log(
+          messages.database.success.role_insert.replace(
+            "{role_name}",
+            role.role_name
+          )
+        );
+      } else {
+        console.log(
+          messages.database.warnings.skipped_role_insert.replace(
+            "{role_name}",
+            role.role_name
+          )
+        );
+      }
+    }
+  }
+
+  // Seed Method Table
+  async seedMethodTable() {
+    const methods = [
+      {
+        method_name: "GET",
+      },
+      {
+        method_name: "POST",
+      },
+      {
+        method_name: "PATCH",
+      },
+      {
+        method_name: "DELETE",
+      },
+      // {
+      //   method_name: "OPTIONS", needed?
+      // }
+    ];
+
+    for (const method of methods) {
+      // Check if method already exists
+      const existingMethod = await this.get(
+        messages.database.queries.select.check_method_exists,
+        [method.method_name]
+      );
+
+      if (!existingMethod) {
+        // Insert the method into the database
+        await this.run(messages.database.queries.insert.method, [
+          method.method_name,
+        ]);
+        console.log(
+          messages.database.success.method_insert.replace(
+            "{method_name}",
+            method.method_name
+          )
+        );
+      } else {
+        console.log(
+          messages.database.warnings.skipped_method_insert.replace(
+            "{method_name}",
+            method.method_name
+          )
+        );
+      }
+    }
+  }
+
+  // Seed Endpoint Table
+  async seedEndpointTable() {
+    const endpoints = [
+      {
+        method_id: Method.GET, // method_id 1 is GET
+        endpoint_name: "/getAllUsersData",
+      },
+      {
+        method_id: Method.POST, // method_id 2 is POST
+        endpoint_name: "/register",
+      },
+      {
+        method_id: Method.POST,
+        endpoint_name: "/login",
+      },
+      {
+        method_id: Method.POST,
+        endpoint_name: "/requestPasswordReset",
+      },
+      {
+        method_id: Method.POST,
+        endpoint_name: "/resetPassword",
+      },
+      {
+        method_id: Method.PATCH, // method_id 3 is PATCH
+        endpoint_name: "/updateRole",
+      },
+      {
+        method_id: Method.DELETE, // method_id 4 is DELETE
+        endpoint_name: "/delete",
+      },
+      {
+        method_id: Method.GET,
+        endpoint_name: "/getNumberOfRequestsByEndpoint",
+      },
+      {
+        method_id: Method.GET,
+        endpoint_name: "/getEndpointsCalledByUser",
+      },
+      {
+        method_id: Method.POST,
+        endpoint_name: "/generate-audio",
+      },
+    ];
+
+    for (const endpoint of endpoints) {
+      // Check if endpoint already exists
+      const existingEndpoint = await this.get(
+        messages.database.queries.select.check_endpoint_exists,
+        [endpoint.endpoint_name]
+      );
+
+      if (!existingEndpoint) {
+        // Insert the endpoint into the database
+        await this.run(messages.database.queries.insert.endpoint, [
+          endpoint.method_id,
+          endpoint.endpoint_name,
+        ]);
+        console.log(
+          messages.database.success.endpoint_insert.replace(
+            "{endpoint_name}",
+            endpoint.endpoint_name
+          )
+        );
+      } else {
+        console.log(
+          messages.database.warnings.skipped_endpoint_insert.replace(
+            "{endpoint_name}",
+            endpoint.endpoint_name
+          )
+        );
+      }
+    }
+  }
+
+  async seedRequestTable() {
+    const requests = [
+      {
+        user_id: 1, 
+        endpoint_id: 1,
+        status_code: 900, // Custom status code for testing
+      },
+    ];
+
+    for (const request of requests) {
+      // Check if request already exists
+      const existingRequest = await this.get(
+        messages.database.queries.select.check_request_exists,
+        [request.user_id, request.endpoint_id]
+      );
+
+      if (!existingRequest) {
+        // Insert the request into the database
+        await this.run(messages.database.queries.insert.request, [
+          request.user_id,
+          request.endpoint_id,
+          request.status_code,
+        ]);
+        console.log(
+          messages.database.success.request_insert.replace(
+            "{user_id}",
+            request.user_id
+          )
+        );
+      } else {
+        console.log(
+          messages.database.warnings.skipped_request_insert.replace(
+            "{user_id}",
+            request.user_id
           )
         );
       }
